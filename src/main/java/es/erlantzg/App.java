0989a81da5398af39b17e396c9d9d1c0333a3164
Erlantz50 <erlantzgarcia05@gmail.com>
2025-09-14 13:59:35 +0200

    Ejercicio terminado y correcto con css, fxml y app.java

--- src/main/java/es/erlantzg/App.java
+++ src/main/java/es/erlantzg/App.java
@@ -3,9 +3,8 @@ package es.erlantzg;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -13,15 +12,27 @@ import java.io.IOException;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader loaded = new FXMLLoader(getClass().getResource("/fxml/flowpane12Sep.fxml"));
-        Scene scene = new Scene(loaded.load(), 320, 240);
+    public void start(Stage stage) {
+        try{
+            FXMLLoader loaded = new FXMLLoader(getClass().getResource("/fxml/flowpane12Sep.fxml"));
+            Scene scene = new Scene(loaded.load(), 600, 100);
 
-        scene.getStylesheets().add(getClass().getResource("/css/estilos.css").toExternalForm());
+            // Comprobar que el archivo de css existe y sino mostrar una alerta
+            var archivoCSS = getClass().getResource("/css/estilos.css");
+            if(archivoCSS != null){
+                scene.getStylesheets().add(archivoCSS.toExternalForm());
+            } else{
+                new Alert(Alert.AlertType.INFORMATION, "No se ha podido cargar el CSS").showAndWait();
+            }
 
-        stage.setTitle("FlowPane Alignment");
-        stage.setScene(scene);
-        stage.show();
+            stage.setTitle("FlowPane Alignment");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, "Error al intentar cargar la aplicación").showAndWait();
+        }
     }
 
     public static void main(String[] args) {
